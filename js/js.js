037f3387fbedde094f81324045496ad841f5d3a1
Clara Cano <mclara.cano@gmail.com>
2021-12-31 16:57:51 -0300

    primer commit 31.12.21

---
+++ js/js.js
@@ -0,0 +1,22 @@
+// let opcionUsuario = prompt(`
+// Elija una opción:
+// 1: Libros
+// 2: Películas
+// 3: Juegos
+// `);
+// console.log(opcionUsuario);
+
+// switch(opcionUsuario){
+//     case "1": 
+//     console.log("Las enseñanzas de Don JUan")
+//     break;
+//     case "2":
+//         console.log("Into the wild")
+//         break;
+//          case "3":
+//              console.log("Famnily Game")
+//              break;
+//              default:
+//                  console.log("Opción no válida")
+//                  break;
+// }
